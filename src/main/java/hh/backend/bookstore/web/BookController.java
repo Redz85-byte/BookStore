65b1951b0035ba6ff89f62bf16bc6d81eacb2550
Redz85-byte <reda.khairy@outlook.com>
2025-02-08 13:48:36 +0200

    Lisäsyn addbookhtml ja lisäys ja delete nappulat

--- src/main/java/hh/backend/bookstore/web/BookController.java
+++ src/main/java/hh/backend/bookstore/web/BookController.java
@@ -4,8 +4,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import hh.backend.bookstore.domain.Book;
 import hh.backend.bookstore.domain.BookRepository;
+
+
+
 
 
 
@@ -22,6 +29,25 @@ public class BookController {
     model.addAttribute("books", bookRepository.findAll());
        return "booklist";
    }
+
+   @GetMapping("/addbook")
+   public String showAddBookForm(Model model) {
+       model.addAttribute("book", new Book());
+       return "addbook";
+   }
+
+   @PostMapping("/addbook")
+   public String addbook(@ModelAttribute Book book) {
+    bookRepository.save(book);
+    return "redirect:/booklist";
+   }
+
+   @GetMapping("/deletebook/{id}")
+   public String deleteBook(@PathVariable("id") Long id) {
+    bookRepository.deleteById(id);
+    return "redirect:/booklist";
+   }
+   
    
 
 }
